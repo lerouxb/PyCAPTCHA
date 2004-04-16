9b1652ea7b3766c921aee6e4408b0779fdee9927
micah <micah@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2004-04-16 11:39:53 +0000

    Add a mod_python example
    git-svn-id: http://svn.navi.cx/misc/trunk/pycaptcha@3724 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import os
+
+# Recursively find all data files
+dataList = []
+def addFiles(results, dir="Captcha/data"):
+    files = []
+    for fileName in os.listdir(dir):
+        if fileName.startswith(".") or fileName.endswith("~"):
+	    continue
+        filePath = os.path.join(dir, fileName)
+        if os.path.isdir(filePath):
+	    addFiles(results, filePath)
+	else:
+	    files.append(filePath)
+    results.append((dir, files))
+addFiles(dataList)
+
 setup (name = "PyCAPTCHA",
        version = "0.2-pre",
        description = "A Python framework for CAPTCHA tests",
@@ -7,8 +24,9 @@ setup (name = "PyCAPTCHA",
        maintainer_email = "micah@navi.cx",
        license = "LGPL",
        packages = [
-    'Captcha',
-    'Captcha.Visual',
-    ],
+           'Captcha',
+           'Captcha.Visual',
+       ],
+       data_files = dataList,
        )
 
